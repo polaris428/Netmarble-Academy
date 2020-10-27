ca0d2a0daa880919ef88ea06ad49138b48f7a400
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-10-28 07:31:28 +0900

    플레이어이동
    조이스틱을 이용한 이동

---
+++ Assets/UIButtotn.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using System.Collections;
+public class UIButtotn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void S()
+    {
+        
+    }
+    public void Jumpbtn(){
+        Debug.Log("adf");
+    }
+}
