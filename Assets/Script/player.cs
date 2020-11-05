357b3b833bcd05d360c4f8b1acee8b343aa8bb7a
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-11-05 17:56:30 +0900

    ㅇㅅㅇ
    ㅇㅅㅇ

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -79,4 +79,11 @@ public class player : MonoBehaviour
        
         
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("sdf");
+        }
+    }
 }
