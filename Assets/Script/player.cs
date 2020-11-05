7628afb61b7c6155defbfc4b32a750ef02dece63
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-11-05 17:12:27 +0900

    애니메이션 작업

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -1,61 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-
-    
 public class player : MonoBehaviour
-{   
-    
-    
-     // 2초 뒤에 실행 } }
-    public float movePower = 1f;
-    public float jumpPower = 1f;
-    
-    Rigidbody2D rigidbody;
-
-    Vector3 movement;
-    bool isjumping = false;
-    // Start is called before the first frame update
-    void Start()
+{
+    public float maxSpeed;
+    public float Jumppow;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    void Awake()
     {
-        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    void jump()
+    {
+        anim.SetBool("isJumping", false);
+    }
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump")&& !anim.GetBool("isJumping"))
         {
-            isjumping = true;
+            rigid.AddForce(Vector2.up * Jumppow, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+            Invoke("jump", 1);
+            
         }
-    }
-    private void FixedUpdate()
-    {
-        Move();
-        Jump();
-    }
-    void Move()
-    {
-        Vector3 moveVelocity = Vector3.zero;
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            moveVelocity = Vector3.left;
-        }else if(Input.GetAxisRaw("Horizontal") > 0)
-        {
-            moveVelocity = Vector3.right;
-        }
-        transform.position += moveVelocity * movePower * Time.deltaTime;
-    }
-    void Jump()
-    {
-        if (!isjumping)
-            return;
-        rigidbody.velocity = Vector2.zero;
 
-        Vector2 jumpVelocity = new Vector2(0, jumpPower);
-        rigidbody.AddForce(jumpVelocity, ForceMode2D.Impulse);
-      
-        isjumping = false;
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        }
+        if (Mathf.Abs( rigid.velocity.x )<0.3 )
+        
+            anim.SetBool("iswarking", false);
+        
+        else
+        
+            anim.SetBool("iswarking", true);
+        
+       
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if(rigid.velocity.x > maxSpeed)
+        
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed*(-1))
+        
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1,LayerMask.GetMask("Tilemap"));
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            
+            if (rayHit.collider != null)
+                    {
+                        if (rayHit.distance < 0.5f)
+                            anim.SetBool("isJumping", false);
+                    }
+        }
+        
+       
+        
     }
 }
