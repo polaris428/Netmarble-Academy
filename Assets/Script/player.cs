c2da53beb44a80036743dbca7df4c32ce4f43124
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-11-15 03:21:28 +0900

    가드 애니메이션 로봇 애니메이션 및 각종 작업

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -7,11 +7,23 @@ public class player : MonoBehaviour
 {
     public float maxSpeed;
     public float Jumppow;
+
+    public float Dashpow;
+    public float startDashTime;
+
+    float currenDashTimer;
+    float DashDirecttion;
+
+    bool isDashing;
+
+    public float movX;
+    
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
     void Awake()
     {
+        
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
@@ -34,35 +46,78 @@ public class player : MonoBehaviour
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            
+            
 
         }
         if (Input.GetButtonDown("Horizontal"))
         {
+            
+            
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            movX = -1;
+            
 
         }
-        if (Mathf.Abs( rigid.velocity.x )<0.3 )
+        
+        if (Mathf.Abs( rigid.velocity.x )<0.1)
         
             anim.SetBool("iswarking", false);
         
         else
         
             anim.SetBool("iswarking", true);
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {    anim.SetBool("isDash", true);
+
+            isDashing = true;
+            currenDashTimer = startDashTime;
+            rigid.velocity = Vector2.zero;
+            DashDirecttion = movX;
+
+
+
+
+            
+        }
+        if (isDashing)
+        {
+            rigid.velocity = transform.right * DashDirecttion * Dashpow;
+
+            currenDashTimer -= Time.deltaTime;
+            if (currenDashTimer <= 0)
+            {
+                isDashing = false;
+                anim.SetBool("isDash", false);
+            }
+        }
        
     }
     void FixedUpdate()
     {
+
+        
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
         if(rigid.velocity.x > maxSpeed)
-        
+        {
+            movX = 1;
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-
-        else if (rigid.velocity.x < maxSpeed*(-1))
+           
+        }
         
+            
+
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            movX =- 1;
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+        }
+        
+            
 
         RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1,LayerMask.GetMask("Tilemap"));
         if (rigid.velocity.y < 0)
@@ -76,7 +131,7 @@ public class player : MonoBehaviour
                     }
         }
         
-       
+      
         
     }
     void OnCollisionEnter2D(Collision2D collision)
--- Assets/robot.cs
+++ Assets/robot.cs
@@ -5,37 +5,104 @@ using UnityEngine;
 
 public class robot : MonoBehaviour
 {
-    public float movePower = 1f;
-    Animator animator;  
-    Vector3 movement;
-    int moevemenFlag = 0;
+  
 
     // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+
+    public float movePower;
+    Animator animator;
+    Vector3 movement;
+    int movementFlag = 0;
+    bool isTracing;
+    GameObject traceTarget;
+
+
+    // Use this for initialization
     void Start()
     {
-        animator = gameObject.GetComponentInChildren<Animator>();
-        StartCoroutine("ChangeMovment");
+        animator = GetComponentInChildren<Animator>();
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);
+
+        
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine("ChangeMovement");
     }
 
     // Update is called once per frame
-    
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
+
     void Move()
     {
         Vector3 moveVelocity = Vector3.zero;
-        if (moevemenFlag == 1)
+        string dist = "";
+
+        if (isTracing)
+        {
+            Vector3 playerPos = traceTarget.transform.position;
+
+            if (playerPos.x < transform.position.x)
+                dist = "Left";
+            else if (playerPos.x > transform.position.x)
+                dist = "Right";
+        }
+        else
+        {
+            if (movementFlag == 1)
+                dist = "Left";
+            else if (movementFlag == 2)
+                dist = "Right";
+        }
+
+        if (dist == "Left")
         {
             moveVelocity = Vector3.left;
             transform.localScale = new Vector3(1, 1, 1);
-
         }
-        else if(moevemenFlag == 2){
+        else if (dist == "Right")
+        {
             moveVelocity = Vector3.right;
-            transform.localScale = new Vector3(-1, -1, -1);
-        }
+            transform.localScale = new Vector3(-1, 1, 1);
+        };
+
         transform.position += moveVelocity * movePower * Time.deltaTime;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            traceTarget = other.gameObject;
+
+            StopCoroutine("ChangeMovement");
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            isTracing = true;
+            animator.SetBool("isMoving", true);
+
+
+        }
+
+
+    }
+    
 }
